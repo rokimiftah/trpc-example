7db42900025c214f1d2168412df26415c111b2ae
Roki Miftah Kamaludin <rokimiftah@gmail.com>
2023-01-26 00:54:08 +0700

    initial commit

---
+++ src/client.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+
+import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
+
+import { AppRouter } from './server';
+
+const client = createTRPCProxyClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:3000/trpc',
+      headers: {
+        authorization: '0',
+      },
+    }),
+  ],
+});
+
+async function main() {
+  await client.getUserById
+    .query({ id: 0 })
+    .then((user) => console.log('Get user by id:', user));
+  await client.createUser
+    .mutate({ name: 'Bob' })
+    .then((user) => console.log('Created user:', user));
+  await client.listUsers // client.users.list
+    .query()
+    .then((users) => console.log('List of users:', users));
+}
+
+main();
